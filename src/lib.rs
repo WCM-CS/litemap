1154796e7cbcae8d9b514813eb9f8ca8b14b77ab
WCM-CS <walker.educs@gmail.com>
2026-01-14 09:48:47 -0500

    const pointer

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use ph::{
     },
     seeds::BitsFast,
 };
-use std::{hash::Hash};
+use std::{hash::Hash, marker::PhantomData};
 
 pub struct FrozenMap<K, V>
 where
@@ -22,7 +22,7 @@ type Mphf =
 
 pub struct FrozenIndex<K>
 where
-    K: Hash + Eq + Clone + Send + Sync + Default,
+    K: Hash + Eq + Send + Sync + Clone + Default,
 {
     pub mphf: Mphf,
     pub keys: Store<K>,
@@ -57,9 +57,11 @@ where
         let mut sorted_values: Vec<V> = vec![V::default(); values.len()];
     
 
-        for (i, (k, v)) in keys.into_iter().zip(values.into_iter()).enumerate() {
-            sorted_keys[i] = k;
-            sorted_values[i] = v;
+        for (k, v) in keys.into_iter().zip(values.into_iter()) {
+            let idx = index_map.get(&k); 
+
+            sorted_keys[idx] = k;
+            sorted_values[idx] = v;
         }
 
         let frozen_index = FrozenIndex {
@@ -92,21 +94,7 @@ where
         }
     }
 
-    #[inline]
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        let idx = self.index.mphf.get(key);
-
-        match self.index.keys.get_value(idx) {
-            Some(k) => {
-                if k == key {
-                    self.store.get_mut_value(idx)
-                } else {
-                    None
-                }
-            },
-            None => None,
-        }
-    }
+ 
 
     #[inline]
     pub fn contains(&self, key: &K) -> bool {
@@ -125,56 +113,50 @@ where
 
 
 pub struct Store<V>
-where
-    V: Send + Sync + Clone,
 {
-    pub value_ptr: *mut V,
-    pub len: usize
+    pub value_ptr: *const V,
+    pub len: usize,
+    _marker: PhantomData<V>
 }
 
+unsafe impl<V: Sync> Sync for Store<V> {}
+unsafe impl<V: Send> Send for Store<V> {}
+
 impl<V> Store<V>
-where
-    V: Send + Sync + Clone,
 {
     #[inline]
     pub fn new(values: Vec<V>) -> Self {
-        let mut values = values;
         let n = values.len();
 
-        let value_ptr = values.as_mut_ptr();
+        let value_ptr = values.as_ptr();
         std::mem::forget(values); // so it doesnt drop from mem after var is descoped
 
 
-        Self { value_ptr, len: n }
+        Self { value_ptr, len: n, _marker: PhantomData}
     }
 
     #[inline]
     pub fn get_value(&self, idx: usize) -> Option<&V> {
-        if idx >= self.len || self.value_ptr.is_null() { // check index bounds and value
+        if idx >= self.len { // check index bounds and value
             None
         } else {
             unsafe { Some( &*self.value_ptr.add(idx) ) }
         }
     }
-
-    #[inline]
-    pub fn get_mut_value(&mut self, idx: usize) -> Option<&mut V> {
-        if idx >= self.len || self.value_ptr.is_null() {
-            None
-        } else {
-            unsafe { Some(&mut *self.value_ptr.add(idx) ) }
-        }
-    }
 }
 
 impl<V> Drop for Store<V>
-where
-    V: Send + Sync + Clone,
 {
     fn drop(&mut self) {
-        if !self.value_ptr.is_null() {
+        if self.len != 0 {
             // assumes data is full otherwise undefined behaviore occcurs, so use unsafe init and dont delete vals until changed to store capacity in store with current len
-            unsafe { let _ = Vec::from_raw_parts(self.value_ptr, self.len, self.len); }
+            unsafe { 
+                let _ = Vec::from_raw_parts(
+                    self.value_ptr as *mut V, 
+                    self.len, 
+                    self.len
+                ); 
+            }
         }
     }
 }
